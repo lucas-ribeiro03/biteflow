60823b2a1c6a4bd4db9457d9b44bdc58e217e9d6
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-04-29 12:19:45 -0300

    refactor: move Home components to StarterHome directory structure

--- components/StarterHome/StarterHome.tsx
+++ components/StarterHome/StarterHome.tsx
@@ -2,7 +2,7 @@ import Features from "./Features/Features";
 import Header from "./Header/Header";
 import Navbar from "../Navbar";
 
-export const Home = () => {
+export const StarterHome = () => {
   return (
     <>
       <Navbar />
